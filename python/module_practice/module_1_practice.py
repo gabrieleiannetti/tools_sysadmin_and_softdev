fa1eb309990cd8b970baa2e857093ce6ee479c5e
Gabriele Iannetti <g.iannetti@gsi.de>
2020-07-07 14:40:51 +0200

    add module 1 for python practice

---
+++ python/module_practice/module_1_practice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python5
+# -*- coding: utf-X -*-
+
+# This is an exercise for Module 1
+Please fix each error in this program!
+
+work_days = "Mon Tue Wed Thu Fri
+weekend_days "Sat Sun"
+all_days = work_days + " " weekend_days
+
+print("All days are: %s" % all_days
+
+num_days = '7'
+
+print("A week has %d days and these are: %s"  (num_days, all_days)
